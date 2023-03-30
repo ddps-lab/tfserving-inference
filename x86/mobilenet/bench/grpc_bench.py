7d2af300284c2ce74847b4cd20b1d99114a0a1a8
mhsong <mhsong@kookmin.ac.kr>
2023-03-30 14:23:20 +0900

    Implemented the ability to input time data into Google Spreadsheets.

--- x86/mobilenet/bench/grpc_bench.py
+++ x86/mobilenet/bench/grpc_bench.py
@@ -3,26 +3,33 @@ import sys
 sys.path.append('../../../bench')
 import module_grpc
 from tensorflow_serving.apis import predict_pb2
+
 #tf log setting
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 #image 전처리 library
 import tensorflow as tf
 import numpy as np
 from PIL import Image
+
 #병렬처리 library
 import concurrent.futures
+
 #시간 측정 library
 import time
-#google sheet api 관련 library
-import module_gspread
+
+#sheet 데이터 입력 모듈
+import put_data_into_sheet
 
 
 # 병렬 처리할 작업 횟수 지정
 num_tasks = 30
 
+# 저장할 google spread sheet id
+spreadsheet_id = ''
 # 모델 서버의 주소 및 포트 번호, 모델 이름 설정
-server_address = 'localhost:8500'
+server_address = ''
 #https 사용시 1로 설정, http 사용시 0으로 설정
 usehttps = 1
 model_name = "mobilenet_v1"
@@ -31,14 +38,18 @@ image_file_path = "../../../dataset/imagenet/imagenet_1000_raw/n01843383_1.JPEG"
 stub = module_grpc.create_grpc_stub(server_address, usehttps)
 
 # 이미지 로드 및 전처리 (for mobilenet)
-img = Image.open(image_file_path)
-img = img.resize((224, 224))
-img_array = np.array(img)
-img_array = img_array.astype('float32') / 255.0
-img_array = np.expand_dims(img_array, axis=0)
+def img_preprocessing(image_file_path):
+    img = Image.open(image_file_path)
+    img = img.resize((224, 224))
+    img_array = np.array(img)
+    img_array = img_array.astype('float32') / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+    data = tf.make_tensor_proto(img_array)
+    return data
+
+data = img_preprocessing(image_file_path)
 
 # gRPC 요청 생성
-data = tf.make_tensor_proto(img_array)
 request = predict_pb2.PredictRequest()
 request.model_spec.name = model_name
 request.model_spec.signature_name = 'serving_default'
@@ -52,7 +63,6 @@ def predict():
     elapsed_time = response_time - request_time
     return result, elapsed_time
 
-all_inference_start_time = time.time()
 # gRPC 요청 병렬 처리
 with concurrent.futures.ThreadPoolExecutor(max_workers=num_tasks) as executor:
     futures = [executor.submit(lambda: predict()) for _ in range(num_tasks)]
@@ -62,16 +72,5 @@ inference_times_include_network_latency = []
 for future in concurrent.futures.as_completed(futures):
     result, thread_elapsed_time  = future.result()
     inference_times_include_network_latency.append(thread_elapsed_time)
-    # print("Thread execution time: ", thread_elapsed_time)
 
-all_inference_end_time = time.time()
-all_inference_elapsed_time = all_inference_end_time - all_inference_start_time
-
-np_inference_times_include_network_latency = np.array(inference_times_include_network_latency)
-
-# print(inference_times_include_network_latency)
-print("all_elapsed_time",all_inference_elapsed_time)
-print("maxtime:",np.max(np_inference_times_include_network_latency))
-print("mintime:",np.min(np_inference_times_include_network_latency))
-print("avgtime:",np.average(np_inference_times_include_network_latency))
-
+put_data_into_sheet.put_data(spreadsheet_id, inference_times_include_network_latency, num_tasks)
--- x86/mobilenet/bench/rest_bench.py
+++ x86/mobilenet/bench/rest_bench.py
@@ -1,46 +1,61 @@
-import time
+#image 전처리 library
 import numpy as np
 from PIL import Image
-#REST Import
+
+#시간 측정 library
+import time
+
+#REST 요청 관련 library
 import requests
 import json
+
+#병렬처리 library
 import concurrent.futures
 
-serving_url = "http://localhost:8501/v1/models/"
+#sheet 데이터 입력 모듈
+import sys
+sys.path.append('../../../bench')
+import put_data_into_sheet
+
+# 병렬 처리할 작업 횟수 지정
+num_tasks = 30
+
+# 저장할 google spread sheet id
+spreadsheet_id = ''
+# protocol을 포함한 URI 형태로 작성
+server_address = "http://localhost:8501/v1/models/"
 model_name = "mobilenet_v1"
 image_file_path = "../../../dataset/imagenet/imagenet_1000_raw/n01843383_1.JPEG"
 
-def send_request(img_array,data):
-    start_time = time.time()
+def send_request(data):
     headers = {"content-type": "application/json"}
-    url = serving_url + model_name + ":predict"
+    url = server_address + model_name + ":predict"
+    request_time = time.time()
     response = requests.post(url, data=data, headers=headers)
-    end_time = time.time()
+    response_time = time.time()
+    elapsed_time = response_time - request_time
     result = json.loads(response.text)
-    return result, end_time - start_time
+    return result, elapsed_time
 
-img = Image.open(image_file_path)
-img = img.resize((224, 224))
-img_array = np.array(img)
-img_array = img_array.astype('float32') / 255.0
-img_array = np.expand_dims(img_array, axis=0)
-data = json.dumps({"instances": img_array.tolist()})
+def img_preprocessing(image_file_path):
+    img = Image.open(image_file_path)
+    img = img.resize((224, 224))
+    img_array = np.array(img)
+    img_array = img_array.astype('float32') / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+    data = json.dumps({"instances": img_array.tolist()})
+    return data
 
-n_requests = 100  # 요청 횟수 변수 선언
+data = img_preprocessing(image_file_path)
 
-start_time = time.time()
+# REST 요청 병렬 처리
+with concurrent.futures.ThreadPoolExecutor(max_workers=num_tasks) as executor:
+    futures = [executor.submit(send_request, data) for _ in range(num_tasks)]
 
-with concurrent.futures.ThreadPoolExecutor(max_workers=n_requests) as executor:
-    futures = [executor.submit(send_request, img_array, data) for _ in range(n_requests)]
-    results = []
-    for future in concurrent.futures.as_completed(futures):
-        result, thread_time = future.result()
-        results.append(result)
-        print("Thread execution time: ", thread_time)
+inference_times_include_network_latency = []
+for future in concurrent.futures.as_completed(futures):
+    result, thread_elapsed_time = future.result()
+    inference_times_include_network_latency.append(thread_elapsed_time)
 
-end_time = time.time()
-response_time = (end_time - start_time)
-
-print("Total response time: ", response_time)
-print(results)
+put_data_into_sheet.put_data(spreadsheet_id, inference_times_include_network_latency, num_tasks)
 
