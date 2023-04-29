3dc55d7d4389beebfb2fafaffcd06fb21d1694a1
kh3654po <kh3654po@gmail.com>
2023-04-29 14:39:57 +0900

    모델 다운로드 코드 및  docker run 파일의 오타 수정

--- arm_edge/docker_run_gRPC.sh
+++ arm_edge/docker_run_gRPC.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+this_pwd=$(pwd)
+
 docker run --rm \
         --gpus all \
         -p 8500:8500 \
-        -v ./models/:/models/ \
+        -v $this_pwd/models/:/models/ \
         edge-tf-serving:latest
